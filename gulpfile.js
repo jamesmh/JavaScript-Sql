f1fa1081731b7b6c3a040c59fe4db5290987999f
James Hickey <jamesmh@users.noreply.github.com>
2016-09-09 17:51:53 -0300

    Added min + uglification

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require('gulp');
 var del = require('del');
 var concat = require('gulp-concat');
 var order = require('gulp-order');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', () => {
     del(['dist/sql.js'], { force: true });
-    return gulp.src(['src/**/*.js'])
+    
+    // Combine all files and generate dist/sql.js
+    gulp.src(['src/**/*.js'])
     	.pipe(order([
     		'meta.js',
     		'polyfills.js',
@@ -15,4 +19,11 @@ gulp.task('default', () => {
 		]))
     	.pipe(concat('sql.js'))    	
     	.pipe(gulp.dest('dist/'));
+
+    
+    // Combine all files, uglify, and generate dist/sql.min.js
+    return gulp.src(['dist/sql.js'])
+        .pipe(rename('sql.min.js'))
+        .pipe(uglify())     
+        .pipe(gulp.dest('dist/'));
 });
